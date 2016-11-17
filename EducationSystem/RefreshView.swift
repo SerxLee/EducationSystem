729958f471bbfce996116bab210b723bc1b03807
serxlee <serx.lee@gmail.com>
2016-11-17 22:30:44 +0800

    Removed the stupid strange file that shouldn't be tracked

---
+++ EducationSystem/RefreshView.swift
@@ -0,0 +1,13 @@
+//
+//  RefreshView.swift
+//  EducationSystem
+//
+//  Created by Serx on 16/6/14.
+//  Copyright © 2016年 Serx.Lee. All rights reserved.
+//
+
+import Cocoa
+
+class RefreshView: UIView {
+
+}
